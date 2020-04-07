683d4cf25154ea1cfb760b9986785cce9c376a13
lffattori <luhf.fattori@icloud.com>
2020-04-07 17:52:22 -0300

    Primeiro Swift Package que eu faço

---
+++ AulaSwiftPackage/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  AulaSwiftPackage
+//
+//  Created by Luiza Fattori on 07/04/20.
+//  Copyright © 2020 Luiza Fattori. All rights reserved.
+//
+
+import UIKit
+import reflectium_ipsum
+
+class ViewController: UIViewController {
+    var answer = String()
+
+    @IBAction func generateAnswer(_ sender: Any) {
+        answer = ReflectionGenerator.generatiumReflectium()
+        changeAnswer()
+
+    }
+
+    @IBOutlet weak var answerView: UITextView!
+
+    func changeAnswer (){
+        answerView.text = answer
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
